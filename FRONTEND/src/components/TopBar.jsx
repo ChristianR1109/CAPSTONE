c4a2d24a3d6336d9e28f74eceae2a0b9b0c03113
Christian Ragonese <christianragonese1313@gmail.com>
2025-09-15 13:15:35 +0200

    navbar

--- FRONTEND/src/components/TopBar.jsx
+++ FRONTEND/src/components/TopBar.jsx
@@ -1,13 +1,93 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, Row, Col } from "react-bootstrap";
 
 const TopBar = (props) => (
-  <Navbar expand="lg" className="nav-bg">
-    <Container className="ms-0">
-      <Navbar.Brand href="#home">EASYTICKETS {props.claim}</Navbar.Brand>
+  <Navbar expand="lg" className="nav-bg flex-column">
+    <Container className="ms-5 ">
+      <Navbar.Brand className="text-white" href="#home">
+        EASYTICKETS {props.claim}
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto"></Nav>
       </Navbar.Collapse>
+    </Container>
+    <Container className="mt-4">
+      <Row>
+        <Col lg={1}>
+          <div className="p-3 border">1</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">1</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">1</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">1</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">2</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">3</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">4</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">5</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">6</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">7</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">8</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">9</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">10</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">1</div>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={1}>
+          <div className="p-3 border">11</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">12</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">13</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">14</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">15</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">16</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">17</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">18</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">19</div>
+        </Col>
+        <Col lg={1}>
+          <div className="p-3 border">20</div>
+        </Col>
+      </Row>
     </Container>
   </Navbar>
 );
